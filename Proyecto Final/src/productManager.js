ade45dc1e7780ad7ffabf9bab5621bbb293eb514
Florencia Villarrta <florencia.inf@gmail.com>
2023-02-19 14:36:43 -0300

    PF entrega

--- Proyecto Final/src/productManager.js
+++ Proyecto Final/src/productManager.js
@@ -1,4 +1,4 @@
-const fs = require("fs/promises");
+const fs = require("fs");
 
 class ProductManager {
   constructor(path) {
@@ -6,56 +6,58 @@ class ProductManager {
     this.products = [];
   }
 
+  //Getting all the products using and asyn function
   async getProducts() {
     try {
       //Setting the path of the data base
-      const productsDb = await fs.readFile(this.path, "utf-8");
-      //Parsing the data, to see something more readable
+      const productsDb = await fs.promises.readFile(this.path, "utf-8");
+      //Parsing the data to can read it
       return JSON.parse(productsDb);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
-  async getProductsById(id) {
+  async getProductById(id) {
     try {
       //Bringing all the data
       const productsDb = await this.getProducts();
-      const productFound = productsDb.find((product) => product.id === id);
-      console.log(productFound);
+      const productFound = productsDb.find((produt) => produt.id === id);
+      console.error(productFound);
+
       return productFound ? productFound : console.log("Product not found");
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
   async createProduct(product) {
     try {
-      //Bringing all the products from the original array
       const productsDb = await this.getProducts();
-      //   Generating a new id in a different function based on the lenght of the current array
       const id = await this.generateNewId(productsDb);
-      //Pushing the product with the new id
       productsDb.push({ id, ...product });
-      //Creatin or overwriting a product with the new info added to the array
+
       await fs.writeFile(this.path, JSON.stringify(productsDb));
-      return "Product added succesfully ";
+      return "Product added succesfully";
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
   async updateProduct(id, data) {
     try {
       const productsDb = await this.getProducts();
-      const productId = await this.getProductsById(id);
-      //Array
-      let index = productsDb.findIndex((product) => product.id === id);
+      const productId = await this.generateNewId(id);
+
+      let index = productsDb.findIndex((product) => {
+        product.id === id;
+      });
       productsDb[index] = { ...productId, ...data };
+
       await fs.writeFile(this.path, JSON.stringify(productsDb));
-      return console.log(productsDb);
+      console.log(productsDb);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
@@ -68,19 +70,18 @@ class ProductManager {
       console.log(productsFiltered);
       return "Product removed succesfully";
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
-  /* ****************************Support Functions ********************************* */
+  /***************************Support Function ************/
   async generateNewId(products) {
     try {
-      console.log(products.length);
-      if (products.length === 1) return 1;
+      if (products.length === 1) {
+        return 1;
+      }
       return products[products.length - 1].id + 1;
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
   }
 }
 
